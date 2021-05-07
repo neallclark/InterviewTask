39c97af344622a32857b4add3fb91bfd77c88201
Neal Clark <neal.clark@ats-global.com>
2021-05-07 11:40:25 +0100

    1st commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup AlgorithmFactory
+            var algorithmFactory = new AlgorithmFactory();
+            RegisterAlgorithms(algorithmFactory);
+
+            do
+            {
+                // Read in list of integers
+                Console.WriteLine("Enter a list of integers (Space seperated)");
+                var numbers = ReadNumbers();
+
+                // Pick an "algorithm" (min, max, mean, first, last). Assume that these "algorithms" are actually really complex algorithms that need to be abstracted away
+                Console.WriteLine("Enter chosen algorithm names seperated by a space (min max mean first last)");
+                var algorithmNames = ReadAlgorithmNames();
+
+                // TODO: get the algorithms based on the entered names
+                // TODO: run the algorithms
+
+                // Show the result
+                PrintResults(results);
+
+                Console.WriteLine("Quit? (Y/N)");
+                if (Console.ReadLine().Trim().ToLower().Equals("y"))
+                    break;
+            }
+            while (true);
+        }
+
+        static void RegisterAlgorithms(AlgorithmFactory algorithmFactory)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void PrintResults(List<AlgorithmResult> results)
+        {
+            throw new NotImplementedException();
+        }
+
+        static IEnumerable<int> ReadNumbers()
+        {
+            throw new NotImplementedException();
+        }
+
+        static IEnumerable<string> ReadAlgorithmNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Requirements
+The program needs to read in a list of integers (e.g. 10 20 30 40 5 2 7)
+Ask the user for a list of algorithms to use to process them (e.g. min max mean first last)
+Then print the results.
+
+## Notes
+The logic of the algorithm itself isn't important (So min, max, mean, first and last are perfectly acceptable).
+What we are looking for is the structure of the code (So try to include the use of Interface's and Abstract classes). Adding additional algorithms should mean very minimal change to the running of the program
+
